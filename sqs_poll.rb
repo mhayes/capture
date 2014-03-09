42ce2bc73615e0e0ddea3c9003ef364a49bf1dfb
Mark Hayes <mark@deployfx.com>
2014-03-09 11:47:05 -0700

    allow outbound queue to be specified and used

--- sqs_poll.rb
+++ sqs_poll.rb
@@ -2,27 +2,31 @@
 
 require "bundler"
 Bundler.require
-puts Dotenv.load(File.join(File.absolute_path(File.dirname(__FILE__)), ".env"))
 
 require 'json'
 require 'securerandom'
 require 'pathname'
 require 'digest'
 
+Dotenv.load(File.join(File.absolute_path(File.dirname(__FILE__)), ".env"))
+
 ROOT_PATH = Pathname.new(File.absolute_path(File.join(File.dirname(__FILE__))))
 BIN_PATH = ROOT_PATH.join("bin")
 CAPTURES_PATH = ROOT_PATH.join("captures")
 
 class Page
-  def initialize(json)
+  def initialize(sqs, json)
     @id = json["id"]
     @url = json["url"]
     @selector = json["selector"]
     @width = json["width"]
     @uuid = SecureRandom.uuid
+    queue = json["outbound_queue"] || ENV["SQS_OUTBOUND_CAPTURE_QUEUE"]
+    @outbound_queue = sqs.queues[queue]
   end
 
-  attr_reader :uuid, :id, :url, :selector, :width, :depository_url, :depository_md5
+  attr_reader :uuid, :id, :url, :selector, :width, :depository_url, 
+    :depository_md5, :outbound_queue
 
   def cmd
     run_cmd = "#{BIN_PATH.join("webkit2png")} -F -W #{width} -D #{CAPTURES_PATH} -o #{uuid}"
@@ -59,14 +63,12 @@ end
 sqs = AWS::SQS.new
 opts = {wait_time_seconds: 10}
 capture_queue = sqs.queues[ENV["SQS_CAPTURE_QUEUE"]]
-outbound_capture_queue = sqs.queues[ENV["SQS_OUTBOUND_CAPTURE_QUEUE"]]
-outbound_comparison_queue = sqs.queues[ENV["SQS_OUTBOUND_COMPARISON_QUEUE"]]
 
 capture_queue.poll(opts) do |msg|
   puts "CaptureQueue: #{msg.body.inspect}"
-  page = Page.new(JSON.parse(msg.body))
+  page = Page.new(sqs, JSON.parse(msg.body))
   if page.capture!
-    outbound_capture_queue.send_message(page.to_json)
+    page.outbound_queue.send_message(page.to_json)
     puts "Captured: #{page.to_json}"
   end
 end
